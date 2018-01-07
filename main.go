7814b9e77cc8a873c97c6224fd6507efc79c08b0
karolgil <karol.gil@getbase.com>
2018-01-07 22:29:42 +0100

    Can use flags with --user and -u

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import ( // run first
+	"fmt"
+	flag "github.com/ogier/pflag"
+)
 
-func main(){
-	fmt.Println("Hello world!")
+var user string // run second
+				// string variable declaration, available for same package - not python-like global
+
+func init(){ // run third
+			 // treated as app/package "constructor"
+	flag.StringVarP(&user, "user", "u", "", "Specify username")
+}
+
+func main(){ // run fourth,
+			 // special - entrypoint for app
+	flag.Parse()
+	fmt.Println(user)
 }
